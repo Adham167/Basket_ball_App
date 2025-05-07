2c4864406b111d8e845cfb48ca55b645eaed9e39
Adham167 <adhamabdelsalam3040@gmail.com>
2025-05-07 06:31:22 +0300

    create team increment function

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -2,5 +2,18 @@ import 'package:basket_ball_app10/cubit/counter_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit(): super(CounterAIncrementState());
+  CounterCubit() : super(CounterAIncrementState());
+
+  int resultA = 0;
+  int resultB = 0;
+
+  void TeamIncrement({required String team, required int buttonNumber}) {
+    if (team == 'A') {
+      resultA += buttonNumber;
+      emit(CounterAIncrementState());
+    }else{
+      resultB += buttonNumber;
+      emit(CounterBIncrementState());
+    }
+  }
 }
